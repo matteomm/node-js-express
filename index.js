ffe51a4bf1eba7e094a0006b2e1be78cffd244e4
Tortella <matteotortella4@gmail.com>
2023-12-21 07:41:31 +0000

    second change

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.listen(5001, () => console.log('API running on port 5001 - Sajith Example'))
 
-app.get('/', (req,res) => res.json('My API running') )
+app.get('/', (req,res) => res.json('My API running - Hi Sajith') )
